e112451c93349218371762debe172298dba77f8c
luricuss <nickolaynevazhno@yandex.ru>
2025-07-06 23:26:58 +0300

    add methods in TaskManager changed var name taskId to id

--- src/Task.java
+++ src/Task.java
@@ -1,11 +1,15 @@
 public class Task {
     private TaskStatus taskStatus;
     private String taskTitle;
-    private final int taskId;
+    private final int id;
 
     public Task(TaskStatus taskStatus, String taskTitle, int taskId) {
         this.taskStatus = taskStatus;
         this.taskTitle = taskTitle;
-        this.taskId = taskId;
+        this.id = taskId;
+    }
+
+    public int getId() {
+        return id;
     }
 }
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -9,6 +9,10 @@ public class TaskManager {
     private HashMap<Integer, Epic> hashMapEpics;
     private HashMap<Integer, Subtask> hashMapSubtasks;
 
+    private static int getTaskId() {
+        return taskId;
+    }
+
 
     public HashMap<Integer, Task> getHashMapTasks() {
         return hashMapTasks;
@@ -20,5 +24,49 @@ public class TaskManager {
 
     public HashMap<Integer, Subtask> getHashMapSubtasks() {
         return hashMapSubtasks;
+    }
+
+    public void deleteAllTasks() {
+        this.getHashMapTasks().clear();
+    }
+    public void deleteAllEpics() {
+        this.getHashMapEpics().clear();
+    }
+    public void deleteAllSubtasks() {
+        this.getHashMapSubtasks().clear();
+    }
+
+    public Task getTaskById(Integer id){
+        return this.getHashMapTasks().get(id);
+    }
+    public Task getEpicById(Integer id){
+        return this.getHashMapEpics().get(id);
+    }
+    public Task getSubtaskById(Integer id){
+        return this.getHashMapSubtasks().get(id);
+    }
+
+    private static int generateNewTaskId() {
+        return getTaskId() + 1;
+    }
+
+    public void addNewTask(Task task){
+        this.getHashMapTasks().put(generateNewTaskId(), task);
+    }
+    public void addNewEpic(Epic epic){
+        this.getHashMapEpics().put(generateNewTaskId(), epic);
+    }
+    public void addNewSubtask(Subtask subtask){
+        this.getHashMapSubtasks().put(generateNewTaskId(), subtask);
+    }
+
+    public void updateTask(Task newTask) {
+        this.getHashMapTasks().put(newTask.getId(), newTask);
+    }
+    public void updateTask(Epic newEpic) {
+        this.getHashMapEpics().put(newEpic.getId(), newEpic);
+    }
+    public void updateTask(Subtask newSubtask) {
+        this.getHashMapTasks().put(newSubtask.getId(), newSubtask);
     }
 }
