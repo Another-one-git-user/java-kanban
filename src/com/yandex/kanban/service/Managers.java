0923d53e924e396887f37d9f64af0a2530123ec4
luricuss <nickolaynevazhno@yandex.ru>
2025-07-15 20:18:32 +0300

    add private constructor

--- src/com/yandex/kanban/service/Managers.java
+++ src/com/yandex/kanban/service/Managers.java
@@ -1,6 +1,9 @@
 package com.yandex.kanban.service;
 
 public class Managers {
+    private Managers() { //запрещаем создание объектов
+    }
+
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
